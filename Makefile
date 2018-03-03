08a214ce33ab00736e8789b1db3266daa21ebb52
Guilhem Jaber <guilhem.jaber@gmail.com>
2018-03-03 14:06:18 +0000

    Update makefile and main

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-SOURCES = parser.mly lexer.mll pmap.ml syntax.ml type_checker.ml unif.ml logic.ml  symb_red.ml skor.ml tcstruct.ml templogic.ml wts.ml wts_closure.ml main.ml
+SOURCES = parser.mly lexer.mll pmap.ml syntax.ml type_checker.ml logic.ml symb_red.ml skor.ml unif.ml rewrite.ml tcstruct.ml templogic.ml wts.ml wts_closure.ml wts_to_dot.ml main.ml
 RESULT  = syteci
 
 OCAMLYACC = menhir
 
+all: native-code
 
 -include OCamlMakefile
--- main.ml
+++ main.ml
@@ -1,27 +1,28 @@
-open Symb_red
-open Parser
 open Skor
 open Tcstruct
 open Logic
 open Templogic
-open Wts
 open Wts_closure
 
-let get_filename () = 
-   if (Array.length Sys.argv <= 2) then failwith ("Error: you should provide two files containing the programs you want to compare.");
+let get_arguments () = 
+  let size_args = Array.length Sys.argv in
+   if (size_args <= 2) then failwith ("Error: you should provide two files containing the programs you want to compare.");
    if (not (Sys.file_exists Sys.argv.(1))) then failwith ("Error: the file " ^ Sys.argv.(1) ^ " does not exist.");
    if (not (Sys.file_exists Sys.argv.(2))) then failwith ("Error: the file " ^ Sys.argv.(2) ^ " does not exist.");
-   (Sys.argv.(1),Sys.argv.(2))
+   let step1 = if (size_args <= 3) then 0 else (int_of_string Sys.argv.(3)) in
+   let step2 = if (size_args <= 4) then 0 else (int_of_string Sys.argv.(4)) in   
+   (Sys.argv.(1),Sys.argv.(2),step1,step2)
+
 
 let get_term filename = 
  let inBuffer = open_in filename in
  let lineBuffer = Lexing.from_channel inBuffer in
  try let expr = Parser.prog Lexer.token lineBuffer in
-     print_string (Syntax.string_of_exprML expr);
-     print_newline (); 
+(*     print_string (Syntax.string_of_exprML expr);
+     print_newline (); *)
      let ty = Type_checker.typing [] [] expr in
-     print_string (Syntax.string_of_typeML ty);
-     print_newline ();     
+(*     print_string (Syntax.string_of_typeML ty);
+     print_newline ();  *)   
      (expr,ty)
  with
    | Lexer.SyntaxError msg -> failwith ("Parsing Error :" ^ msg)
@@ -29,24 +30,20 @@ let get_term filename =
    | Type_checker.TypingError msg -> failwith ("Type error :" ^ msg)   
    
 let () =
-        let (filename1,filename2) = get_filename () in
-        print_string ("First Program");
-        print_newline ();
+        let (filename1,filename2,step1,step2) = get_arguments () in
         let (expr1,ty1) = get_term filename1 in
-        print_string ("Second Program");
-        print_newline ();
-        let (expr2,ty2) = get_term filename2 in
-        let tc = build_tc_rule [] (emptyctx_sequent (RelE (ty1,[],(expr1,[]),(expr2,[]))) 0 0) in (*(int_of_string Sys.argv.(3))) in *)        
+        let (expr2,_) = get_term filename2 in       
+        let tc = Tcstruct.build_tc ty1 expr1 expr2 step1 step2 in 
         let temp_form = iter 10 simplify_temp_formula (tformula_of_tc tc) in   
         print_endline ("Temporal Formula:");
         print_string (string_of_temp_formula temp_form);
         print_newline ();
         print_endline ("WTS:");
-        let (sr,isExt,isWB,preds) = Wts.build_esr tc in   
-        Wts.print_sr sr;
-        print_endline ("Closed WTS:");        
+        let sr = Wts.build_sr tc in   
+(*        Wts.print_sr sr;
+        print_endline ("Closed WTS:");  *)      
         let sr' = sr_closure sr in
-        Wts.print_sr sr';     
+        Wts_to_dot.dot_from_sr sr';     
 (*        print_endline ("Reachability:");
         let paths = backward_sr sr' in
         List.iter (fun (lists,x) -> ((List.iter (fun s -> print_string ((string_of_state s) ^" ")) lists); (print_endline (string_of_arith_pred x)))) paths*)
